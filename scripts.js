bdc81a3a84a68471be242cc5549412b0812cf8ff
Timothy hall <59980086+halltim91@users.noreply.github.com>
2022-08-07 16:23:32 -0700

    nav menu closes when user clicks outside of menu. bug fixes

--- scripts.js
+++ scripts.js
@@ -1,13 +1,26 @@
 // set up hiding menu when on small screen
-document.addeventlis
+const smallwidth = 768;
+
 let cb = document.getElementById("cbtoggle");
 var btns = document.querySelectorAll(".navlink");
 console.log(btns);
 
 for(var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function(){
-    if (screen.width < 768){
+    if (screen.width < smallwidth){
       cb.checked = false;
     } 
   });
 }
+
+//if we are in small screen mode, and the drop down menu is open
+//close it if user clicks outside of menu
+window.addEventListener('mouseup', function(event){
+  if (screen.width < smallwidth){
+    if (cb.checked === true){
+      if (!event.target.matches('.navlink') || !event.target.matches('.menu')){
+        cb.checked = false;
+      }
+    }
+  }
+})
