1daab53f5e735fa4356f3b5b3805e62e85a85975
Timothy hall <59980086+halltim91@users.noreply.github.com>
2022-08-09 12:37:12 -0700

    Converted nav menu function to JS.  Commented code. Cleaned CSS.

--- scripts.js
+++ scripts.js
@@ -1,40 +1,80 @@
 // set up hiding menu when on small screen
-const smallwidth = 768;
+const smallWidth = 768;
+var showMenu = false;
 
-var cb = document.getElementById("cbtoggle");
+var hamburger = document.getElementById("hamburger");
 var btns = document.querySelectorAll(".navlink");
+var menu = document.getElementById("menu");
+var navlis = document.querySelectorAll(".navli");
 
-for(var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(event){
-    if (screen.width < smallwidth){
-      if(event.button == 0){
-        cb.checked = false;
-      }
-    } 
-  });
+/*Sets the display mode for all of the nav bar list items */
+function setLIdisplay(display) {
+  for (var i = 0; i < navlis.length; i++) {
+    navlis[i].style.display = display;
+  }
 }
-var hamburger = document.getElementById("hamburger")
-var menu = document.getElementById("menu")
 
-hamburger.addEventListener('mouseup', function(event){
-  console.log("hamburger:" + cb.checked);
-  if(cb.checked === true) {
-    cb.checked = false;
-    console.log("hamburger changed: " + cb.checked)
+/* Toggles displaying the menu. */
+function toggleMenu() {
+  if (showMenu) {
+    showMenu = false;
+    menu.style.display = "none";
+    setLIdisplay("none");
+  } else {
+    showMenu = true;
+    menu.style.display = "block";
+    setLIdisplay("block");
+  }
+}
+
+/* Opens/closes menu when hamburger button is clicked */
+hamburger.addEventListener("click", function (event) {
+  if (event.button === 0) {
+    toggleMenu();
   }
 });
 
-//if we are in small screen mode, and the drop down menu is open
-//close it if user clicks outside of menu
-window.addEventListener('mouseup', function(event){
-  console.log("window: " + cb.checked);
-  if (screen.width < smallwidth){
-    if(event.button == 0){
-      if (cb.checked === true){
-        if (!event.target.matches('.navlink') || !event.target.matches('.menu')){
-          cb.checked = false;
+/* Closes menu when user clicks a link*/
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function (event) {
+    if (screen.width < smallWidth) {
+      if (event.button == 0) {
+        toggleMenu();
+      }
+    }
+  });
+}
+
+/*if we are in small screen mode, and the drop down menu is open
+close it if user clicks outside of menu */
+window.addEventListener("mouseup", function (event) {
+  if (screen.width < smallWidth) {
+    if (event.button == 0) {
+      if (
+        !event.target.matches(".navlink") &&
+        !event.target.matches(".hamburger")
+      ) {
+        if (showMenu) {
+          toggleMenu();
         }
       }
     }
   }
-})
+});
+
+var last = window.innerWidth;
+
+/* restore menu style when screen resized back up or down*/
+window.addEventListener("resize", function (event) {
+  if (last <= smallWidth && this.innerWidth > smallWidth) {
+    menu.style.display = "flex";
+    for (var i = 0; i < navlis.length; i++) {
+      navlis[i].style.display = "inline-block";
+    }
+    showMenu = false;
+  }
+  if (last >= smallWidth && this.innerWidth < smallWidth) {
+    menu.style.display = "none";
+  }
+  last = this.innerWidth;
+});
