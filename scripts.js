bc2d8b975c781c6b9145d5c4e2befe89d91cc270
Timothy hall <59980086+halltim91@users.noreply.github.com>
2022-08-08 17:24:09 -0700

    removed console log

--- scripts.js
+++ scripts.js
@@ -3,7 +3,6 @@ const smallwidth = 768;
 
 let cb = document.getElementById("cbtoggle");
 var btns = document.querySelectorAll(".navlink");
-console.log(btns);
 
 for(var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function(){
